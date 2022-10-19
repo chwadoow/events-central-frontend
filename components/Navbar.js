b9112a51ad9aedea87f19b442087bda863d0acf7
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-19 18:24:26 +0300

    edited the navbar component to be rendered on every page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,16 @@
 import {Menu} from "antd"
-import { MailOutlined} from '@ant-design/icons';
+import { MailOutlined, HomeTwoTone} from '@ant-design/icons';
 import Link from "next/link"
 function Navbar(){
 return (
     <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
-    <Menu.Item key="mail" icon={<MailOutlined />}>
+    <Menu.Item key="mail" icon={<HomeTwoTone /> }>
       <Link href="/">Home</Link>
     </Menu.Item>
-    <Menu.Item key="mail" icon={<MailOutlined />}>
+    <Menu.Item key="mail">
       <Link href="/createvent">Create Event</Link>
     </Menu.Item>
-    <Menu.Item key="mail" icon={<MailOutlined />}>
+    <Menu.Item key="mail">
       <Link href="/about">About us</Link>
     </Menu.Item>
     </Menu>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-// import ""
+import 'antd/dist/antd.css';
 import '../styles/globals.css'
-
-
+import Navbar from '../components/Navbar';
+import Layout from 'antd/lib/layout/layout';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <>
+    <Navbar/>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    </>
+  )
 }
 
 export default MyApp
