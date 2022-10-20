7fc43ec8989dd93c1d39d0645df820f62e935ca9
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-20 08:27:56 +0300

    added a login component

--- pages/createvent.js
+++ pages/createvent.js
@@ -1,22 +1,9 @@
-import {Form, Input,Button} from "antd/lib/form/Form"
 
 function createevent(){
 return (
     <>
     <h1>The Create event Component</h1>
-    <div>
-      {/* <Form style={{width: 300, margin: 50, paddingTop: 50}}>
-          <Form.Item label="Username" name=  "username">
-            <Input placeholder='Enter username' required></Input>
-          </Form.Item>
-          <Form.Item label="Password" name = "password">
-            <Input.Password  placeholder='Enter password' required></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button block type='danger' htmlType='submit'>Submit</Button>
-          </Form.Item>
-        </Form> */}
-    </div>
+   
     </>
     
 )
