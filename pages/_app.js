4cc2e1f0854113c1b13947c744b199a2fe59d912
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-20 08:39:00 +0300

    imported the stylesheet in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.css';
 import '../styles/globals.css';
-// import "../styles/login.css";
+import "../styles/login.css";
 import Navbar from '../components/Navbar';
 import Layout from 'antd/lib/layout/layout';
 function MyApp({ Component, pageProps }) {
