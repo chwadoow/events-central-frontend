e9717a4413b56941b79da09e2fcaf0dad34a4ef1
moses ngige <moses32ngigz@gmail.com>
2022-10-20 19:48:21 +0300

    create event form

--- pages/createvent.js
+++ pages/createvent.js
@@ -1,24 +1,85 @@
-import {Form, Input,Button} from "antd/lib/form/Form"
-
-function createevent(){
-return (
-    <>
-    <h1>The Create event Component</h1>
-    <div>
-      {/* <Form style={{width: 300, margin: 50, paddingTop: 50}}>
-          <Form.Item label="Username" name=  "username">
-            <Input placeholder='Enter username' required></Input>
-          </Form.Item>
-          <Form.Item label="Password" name = "password">
-            <Input.Password  placeholder='Enter password' required></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button block type='danger' htmlType='submit'>Submit</Button>
-          </Form.Item>
-        </Form> */}
-    </div>
-    </>
-    
-)
-}
-export default createevent
+import {
+  Button,
+  Cascader,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Switch,
+  TreeSelect,
+} from 'antd';
+import React, { useState } from 'react';
+const App = () => {
+  const [componentSize, setComponentSize] = useState('large');
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+  return (
+    <div className='create'>
+    <Form
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 14,
+      }}
+      layout="horizontal"
+      initialValues={{
+        size: componentSize,
+      }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize}
+    >
+     
+      <Form.Item label="Event Name">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Organiser Name">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Location">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Description">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Category">
+        <Select>
+          <Select.Option value="demo">Art ant Theatre</Select.Option>
+          <Select.Option value="demo">Sports</Select.Option>
+        </Select>
+      </Form.Item>
+      {/* <Form.Item label="TreeSelect">
+        <TreeSelect
+          treeData={[
+            {
+              title: 'Light',
+              value: 'light',
+              children: [
+                {
+                  title: 'Bamboo',
+                  value: 'bamboo',
+                },
+              ],
+            },
+          ]}
+        />
+      </Form.Item> */}
+     
+      <Form.Item label="Event Starting date">
+        <DatePicker />
+      </Form.Item>
+      <Form.Item label="Ticket Number">
+        <InputNumber />
+      </Form.Item>
+     
+      <Form.Item label="Submit">
+        <Button>Button</Button>
+      </Form.Item>
+    </Form>
+   </div>
+  );
+};
+export default App;
