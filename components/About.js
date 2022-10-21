8a1d110e916524505dc3141e4cd141c9486805bf
francis <franciskibira017@gmail.com>
2022-10-21 09:22:36 +0300

    done

--- components/About.js
+++ components/About.js
@@ -2,9 +2,10 @@ const about =() => {
     return (
         <div>
         <Head>
-        <title>About</title>
+        <title>Who are we</title>
         </Head>
-        <h1>About</h1>
+        <h1>Bringing the World together through live experience</h1>
+        <h2>Thunder tick is a global self-service ticketing platform for live experiences that allows anyone to create, share, find and attend events that fuel their passions and enrich their lives. From music festivals, marathons, conferences, community rallies, and fundraisers, to gaming competitions and air guitar contests. Our mission is to bring the world together through live experiences.</h2>
         </div>
     )
 }
