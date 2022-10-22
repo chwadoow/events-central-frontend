3f88af4a55b003b60983892178859384537d2b75
francis <franciskibira017@gmail.com>
2022-10-22 11:32:10 +0300

    done

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
 import { Image } from 'antd';
 import React, { useState } from 'react';
 
-const App: React.FC = () => {
+const about  = () => {
   const [visible, setVisible] = useState(false);
 
   return (
-    <>
+    <div>
+    <h1>Who we are</h1>
+    <p>Bringing the World together through live experience</p>
+    <p1>Thunder tick is a global self-service ticketing platform for live experiences that allows anyone to create, share, find and attend events that fuel their passions and enrich their lives. From music festivals, marathons, conferences, community rallies, and fundraisers, to gaming competitions and air guitar contests. Our mission is to bring the world together through live experiences.</p1>
       <Image
         preview={{ visible: false }}
         width={200}
@@ -19,8 +22,9 @@ const App: React.FC = () => {
           <Image src="https://i.pinimg.com/564x/8c/da/fc/8cdafc67418faf8b9993e830cbc8c6fb.jpg" />
         </Image.PreviewGroup>
       </div>
-    </>
+      </div>
+    
   );
 };
 
-export default App;
+export default about
