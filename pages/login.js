05af76390be612abbeaee094d1c6a4ef481a5a59
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-24 22:22:55 +0300

    defined the POST function that sends data to the db

--- pages/login.js
+++ pages/login.js
@@ -19,6 +19,29 @@ function Login(){
   }; 
 
 
+// will handle POST
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          setCurrentUser(user);
+        });
+      } else {
+        res.json().then((errors) => {
+          console.error(errors);
+        });
+      }
+    });
+  };
+
+
     return (
         <div className="login-page">
       <div className="login-box">
@@ -26,6 +49,7 @@ function Login(){
           <img src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700" alt="Login"/>
         </div>
         <Form
+           onSubmit={handleSubmit}
           name="login-form"
         >
           <p className="form-title">LOG IN</p>
@@ -56,7 +80,8 @@ function Login(){
           </Form.Item>
 
           <Form.Item>
-            <Button  htmlType="submit" className="login-form-button">
+            <Button  
+            htmlType="submit" className="login-form-button">
               LOGIN
             </Button>
           </Form.Item>
