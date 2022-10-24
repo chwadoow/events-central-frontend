671c8aed74b20aa6c270341fe9c61511050522e5
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-24 23:59:31 +0300

    Added data to be rendered in the event card home page

--- components/HomePageEvents.js
+++ components/HomePageEvents.js
@@ -1,8 +1,11 @@
-import { Col, Card, Row } from "antd";
 import Link from "next/link";
+import { Col, Card, Row } from "antd";
 
 const HomePageEvents = ({events}) => {
-
+  var date = new Date()
+  console.log(date)
+  var dd = date.getTime()
+  console.log(dd)
 return (
     <Row>
       <Col span={24}>
@@ -12,8 +15,12 @@ return (
                 <>
                   <Link href={`/events/${event.id}`}>
                     <div>
-                      <Card style={{ textAlign: "center", width: 280, height: 50, padding: 2, cursor: "pointer", borderRadius: 10}}>
+                      <Card style={{ textAlign: "center", width: 280, height: 50, padding: 2, cursor: "pointer", borderRadius: 10}}
+                        cover={<img alt="example" src={event.image_url1} />}>
                       <p><b>{event.title}</b></p>
+                      <p><b>{event.location}</b></p>
+                      <p><b>{`${event.event_date}`.split("-").join("/")}</b></p>
+                      <p><b>{`${event.event_time}`.split("2000-01-01T")}</b></p> 
                       </Card>
                     </div>
                   </Link>
