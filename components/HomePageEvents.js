bf086bb3d47c5abbfce85ff0a968ddc61c228b9a
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-25 11:27:09 +0300

    Implemented login functionality

--- components/HomePageEvents.js
+++ components/HomePageEvents.js
@@ -4,11 +4,14 @@ import { Col, Card, Row } from "antd";
 const HomePageEvents = ({events}) => {
   var date = new Date()
   var currentDate = date.getTime()
+  const filteredDates = events.filter((event)=> 
+     (parseInt(((new Date(`${event.event_date}`.split("-").join("/")).getTime()) - currentDate )/(1000 * 60 * 60 * 24)) > 0)
+    )
 return (
     <Row>
       <Col span={24}>
         <div style={{ marginRight: 10, marginLeft: 10, borderRadius: 20, gap: 10, justifyContent: "center", flexWrap: "wrap", display: "inline-flex", flexDirection: 'row' }}>
-          {events.map((event) => {
+          {filteredDates.map((event) => {
               return (
                 <>
                   <Link href={`/events/${event.id}`}>
