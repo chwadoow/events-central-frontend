78717fe460fbb9eb5226a8cbe40efbc6d4baf06d
francis <franciskibira017@gmail.com>
2022-10-25 15:07:39 +0300

    done

---
+++ pages/userprofile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function userprofile() {
+  return (
+    <div>userprofile
+    <Form.Item label="user_id">
+        <Input />
+      </Form.Item>
+      <Form.Item label="full_name">
+        <Input />
+      </Form.Item>
+      <Form.Item label="avatar_img">
+        <Input />
+      </Form.Item>
+      </div>
+    
+  )
+}
+
+export default userprofile
