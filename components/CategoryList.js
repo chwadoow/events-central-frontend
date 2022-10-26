bc57746b836eba93488b7659a14bdff0a1fdb9d5
Keith <keithwesley187@gmail.com>
2022-10-26 17:04:23 +0300

    milestone: styled category list

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -10,7 +10,17 @@ return (
               return (
                 <div key={event.id}>
                   <Link href={`/eventcategories/${event.id}`}>
-                    <div style={{ textAlign: "center", border: 1, borderStyle: "solid", width: 280, height: 50, padding: 2, cursor: "pointer", borderRadius: 10}}>
+                    <div style={{
+                       textAlign: "center", 
+                       border: 1, 
+                       borderStyle: "solid", 
+                       width: 280, 
+                       height: 50, 
+                       padding: 2, 
+                       cursor: "pointer", 
+                       borderRadius: 10,
+                       boxShadow: "5px 5px #d1410a"
+                       }}>
                       <p style={{marginTop: 10}}><b>{event.category}</b></p>
                     </div>
                   </Link>
