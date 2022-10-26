e8d73146d1d1a1960a09df9a3c288b6c63327d36
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-26 11:46:37 +0300

    added a ticket counter

--- components/BuyTicketForm.js
+++ components/BuyTicketForm.js
@@ -4,33 +4,24 @@ import React, { useState } from 'react';
   const BuyTicketForm = ({loading, onClick, event}) => {
     const [vipTickets, setVipTickets] = useState(0)
     const [regularTickets, setRegularTickets] = useState(0)
-    const [totalAmount, setTotalAmount] = useState(0)
     const [componentSize, setComponentSize] = useState('default');
     const onFormLayoutChange = ({ size }) => {
       setComponentSize(size);
     };
-    var date = new Date()
-    var currentDate = date.getTime()
-    
+    let date = new Date()
+    let currentDate = date.getTime()
+    let totalAmount = 0
     const conditionedPricingVIP = parseInt(((new Date(`${event.early_booking_end_date}`.split("-").join("/")).getTime()) - currentDate )/(1000 * 60 * 60 * 24)) > 0 ? event.early_booking_price_vip: event.vip_price
 
     const conditionedPricingRegular = parseInt(((new Date(`${event.early_booking_end_date}`.split("-").join("/")).getTime()) - currentDate )/(1000 * 60 * 60 * 24)) > 0 ? event.early_booking_price_regular: event.regular_price
-
-    function handleChange(event){
-      console.log(event.target.value)
-      setTotalAmount((vipTickets * conditionedPricingVIP) + (regularTickets * conditionedPricingRegular))
-    }
-    console.log(event.tickets)
-    const vipTicketCount = event.tickets.reduce((previouseTicketCount, currentTicketCount) => previouseTicketCount + currentTicketCount.number_of_vip_tickets,0);
-
-    const regularTicketCount = event.tickets.reduce((previouseTicketCount, currentTicketCount) => previouseTicketCount + currentTicketCount.number_of_regular_tickets,0);
-
     
-    console.log(vipTicketCount)
+    const vipTicketCount = event.tickets.reduce((previousTicketCount, currentTicketCount) => previousTicketCount + currentTicketCount.number_of_vip_tickets,0);
 
-    console.log(regularTicketCount)
+    const regularTicketCount = event.tickets.reduce((previousTicketCount, currentTicketCount) => previousTicketCount + currentTicketCount.number_of_regular_tickets,0);
 
-
+    totalAmount = (vipTickets * conditionedPricingVIP) + (regularTickets * conditionedPricingRegular)
+    
+    let ticketNumber = vipTicketCount + regularTicketCount
 
     return (
       <Form
@@ -55,27 +46,25 @@ import React, { useState } from 'react';
           </Radio.Group>
         </Form.Item>
         <Form.Item label="Ticket No:">
-        <label>Yet to Input</label>
+        <label>{`GamCOD`+`${ticketNumber + 1}`}</label>
           </Form.Item>
         <Form.Item label="VIP">
         <label>Tickets Remaining</label>
-          <Form.Item label={event.vip_no_of_tickets}>
+          <Form.Item label={event.vip_no_of_tickets - vipTicketCount}>
           </Form.Item>
         <Form.Item label="Price">
         <Form.Item label={conditionedPricingVIP}>
-          <Input  type="number" placeholder="book vip seat" onChange={handleChange}/>
-          {/* // onChange={(event)=> setVipTickets(event.target.value)} */}
+          <Input  type="number" placeholder="book vip seat"  min="0" onChange={(event)=> setVipTickets(event.target.value)}/> 
           </Form.Item>
           </Form.Item>
         </Form.Item>
         <Form.Item label="Regular">
         <label>Tickets Remaining</label>
-        <Form.Item label={event.regular_no_of_tickets}>
+        <Form.Item label={event.regular_no_of_tickets - regularTicketCount}>
           </Form.Item>
           <Form.Item label="Price">
         <Form.Item label={conditionedPricingRegular}>
-          <Input placeholder="booked regular number of seats " type="number" onChange={handleChange}/>
-          {/* //  onChange={(event)=> setRegularTickets(event.target.value)} */}
+          <Input placeholder="booked regular number of seats " type="number" min="0" onChange={(event)=> setRegularTickets(event.target.value)} />
           </Form.Item>
           </Form.Item>
         </Form.Item>
