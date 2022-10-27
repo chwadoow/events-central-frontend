1ff18776ac17c27afd943a5e8e904711901c999c
moses ngige <moses32ngigz@gmail.com>
2022-10-27 20:40:23 +0300

    user card design

---
+++ components/UserCard.js
@@ -0,0 +1,104 @@
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+import { Avatar, Card, Skeleton, Switch } from 'antd';
+
+const { Meta } = Card;
+function  UserCard  ()  {
+  return (
+ <>
+
+   <Card
+      style={{
+        marginTop: 16,
+        width: "100%",
+        boxShadow: "5px 5px 5px #888" 
+      }}
+   
+    >
+      <div
+      style={{ float:"left"}}
+      >
+      full name
+      </div>
+<div
+style={{ float:"right"}}
+>
+moz
+</div>
+   
+    </Card>
+
+
+
+   <Card
+      style={{
+        marginTop: 16,
+        width: "100%",
+        boxShadow: "5px 5px 5px #888" 
+      }}
+   
+    >
+      <div
+      style={{ float:"left"}}
+      >
+    phone
+      </div>
+<div
+style={{ float:"right"}}
+>
+0799999999
+</div>
+   
+    </Card>
+
+
+
+   <Card
+      style={{
+        marginTop: 16,
+        width: "100%",
+        boxShadow: "5px 5px 5px #888" 
+      }}
+   
+    >
+      <div
+      style={{ float:"left"}}
+      >
+   gender
+      </div>
+<div
+style={{ float:"right"}}
+>
+male
+</div>
+   
+    </Card>
+
+
+
+
+   <Card
+      style={{
+        marginTop: 16,
+        width: "100%",
+        boxShadow: "5px 5px 5px #888" 
+      }}
+   
+    >
+      <div
+      style={{ float:"left"}}
+      >
+      bio
+      </div>
+<div
+style={{ float:"right"}}
+>
+life is short party fast
+</div>
+   
+    </Card>
+</>
+  )
+
+}
+export default UserCard
