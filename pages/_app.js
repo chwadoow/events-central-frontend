95dfca89ccb3de2b98ed3f25644ec2efcd318bad
moses ngige <moses32ngigz@gmail.com>
2022-10-31 15:19:32 +0300

    typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import 'antd/dist/antd.css'; 
 import '../styles/globals.css';
-import "../styles/login.css";
+
 import {useEffect,useState} from "react";
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
 
   return( 
-    <>
+    <div className='app'>
     <Navbar/>
     <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
