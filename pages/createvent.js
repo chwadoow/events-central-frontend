bacb3e1a5e717eff2d26f5e4682613f6391eadcf
Keith <keithwesley187@gmail.com>
2022-10-31 22:39:47 +0300

    milestone: POST for event creation working

--- pages/createvent.js
+++ pages/createvent.js
@@ -1,10 +1,30 @@
-import { Col, Row, DatePicker, Form, Input, Select, Button } from 'antd'
+import { Col, Row, DatePicker, Form, Input, Button } from 'antd'
 import TextArea from 'antd/lib/input/TextArea';
 import React, { useEffect, useState } from 'react'
 
 const createvent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [categoryData, setCategoryData] = useState({});
+  const [formData, setFormData] = useState({
+    category_id: '',
+    title: '',
+    event_start_date: '',
+    event_end_date: '',
+    ticket_format: '',
+    ticket_format: '',
+    early_booking_end_date: '',
+    early_booking_price_regular: '',
+    early_booking_price_vip: '',
+    location: '',
+    regular_price: '',
+    vip_price: '',
+    vip_no_of_tickets: '',
+    regular_no_of_tickets: '',
+    banner_img: '',
+    description: '',
+    image_url1: '',
+    image_url2: ''
+  })
 
   useEffect(()=>{
     fetch(`http://localhost:3000/categories`)
@@ -14,6 +34,46 @@ const createvent = () => {
       setIsLoading(false)
     })
   },[])
+
+  function handleChange(e){
+    setFormData({
+        ...formData, [e.target.name]: e.target.value,
+    });
+  }
+
+  console.log(formData)
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch(`http://localhost:3000/events`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+
+    setFormData({
+      category_id: '',
+      title: '',
+      event_start_date: '',
+      event_end_date: '',
+      ticket_format: '',
+      ticket_format: '',
+      early_booking_end_date: '',
+      early_booking_price_regular: '',
+      early_booking_price_vip: '',
+      location: '',
+      regular_price: '',
+      vip_price: '',
+      vip_no_of_tickets: '',
+      regular_no_of_tickets: '',
+      banner_img: '',
+      description: '',
+      image_url1: '',
+      image_url2: ''
+    })
+  }
 
   if(isLoading === true) return (
     <Row justify="center" align="middle">
@@ -36,6 +96,7 @@ const createvent = () => {
         <Form 
         layout="vertical"
         size='small'
+        onSubmit={handleSubmit}
         >
           <Row justify='center' align='middle'>
 
@@ -45,20 +106,25 @@ const createvent = () => {
                 label="Category"
                 style={{width: "70%"}}
                 required
-                name="category_id"
                 >
-                  <Select>
+                  <select
+                  name='category_id'
+                  onChange={handleChange}
+                  style={{width: "100%"}}
+                  >
+                    <option value="">--Please choose a category--</option>
                     {categoryData.map((category) => {
                       return (
-                        <Select.Option value={category.id}>{category.title}</Select.Option>
+                        <option key={category.id} value={category.id} >
+                          {category.title}
+                        </option>
                       )
                     })}
-                  </Select>
+                  </select>
                 </Form.Item>
                 <br />
                 <Form.Item
                   label="Event Title"
-                  name="title"
                   style={{width: "70%"}}
                   rules={[
                     {
@@ -67,61 +133,63 @@ const createvent = () => {
                     },
                   ]}
                 >
-                  <Input placeholder="eg. BDO tournament"/>
+                  <Input name="title" onChange={handleChange} value={formData.title} placeholder="eg. BDO tournament"/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Event Starting Date"
                 style={{width: "70%"}}
                 required
-                name="event_start_date"
                 >
-                  <DatePicker style={{width: "100%"}} showTime/>
+                  <Input type='datetime-local' name="event_start_date" onChange={handleChange} value={formData.event_start_date} style={{width: "100%"}} />
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Event Ending Date"
                 style={{width: "70%"}}
                 required
-                name="event_end_date"
                 >
-                  <DatePicker style={{width: "100%"}} showTime/>
+                  <Input type='datetime-local' name="event_end_date" onChange={handleChange} value={formData.event_end_date} style={{width: "100%"}} showTime/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Early Booking End Date"
                 style={{width: "70%"}}
                 required
-                name="early_booking_end_date"
                 >
-                  <DatePicker style={{width: "100%"}} showTime/>
+                  <Input type='datetime-local' name="early_booking_end_date" onChange={handleChange} value={formData.early_booking_end_date} style={{width: "100%"}} showTime/>
+                </Form.Item>
+                <br />
+                <Form.Item 
+                label="Ticket Format"
+                style={{width: "70%"}}
+                required
+                >
+                  <Input name="ticket_format" onChange={handleChange} value={formData.ticket_format} placeholder='eg. BDOTourney' type='text'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Early Regular Booking Ticket Price ($)"
                 style={{width: "70%"}}
                 required
-                name="early_booking_price_regular"
                 >
-                  <Input type='number'/>
+                  <Input min={0} name="early_booking_price_regular" onChange={handleChange} value={formData.early_booking_price_regular} type='number'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Early Vip Booking Ticket Price ($)"
                 style={{width: "70%"}}
                 required
-                name="early_booking_price_vip"
                 >
-                  <Input type='number'/>
+                  <Input min={0} name="early_booking_price_vip" onChange={handleChange} value={formData.early_booking_price_vip} type='number'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Regular Price ($)"
                 style={{width: "70%"}}
                 required
-                name="regular_price"
                 >
-                  <Input type='number'/>
+                  <Input min={0} name="regular_price" onChange={handleChange} value={formData.regular_price} type='number'/>
                 </Form.Item>
 
               </Row>
@@ -130,66 +198,67 @@ const createvent = () => {
             <Col span={12}>
               <Row justify='center' align='middle'>
                 <Form.Item 
+                label="Location"
+                style={{width: "70%"}}
+                required
+                >
+                  <Input name="location" onChange={handleChange} value={formData.location} placeholder='eg. Nairobi' type='text'/>
+                </Form.Item>
+                <br />
+                <Form.Item 
                   label="Vip Price ($)"
                   style={{width: "70%"}}
                   required
-                  name="vip_price"
                   >
-                    <Input type='number'/>
+                    <Input min={0} name="vip_price" onChange={handleChange} value={formData.vip_price} type='number'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
-                label="Vip Ticket Number"
+                label="Number of Vip Tickets"
                 style={{width: "70%"}}
                 required
-                name="vip_no_of_tickets"
                 >
-                  <Input type='number'/>
+                  <Input min={0} name="vip_no_of_tickets" onChange={handleChange} value={formData.vip_no_of_tickets} type='number'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
-                label="Regular Ticket Number"
+                label="Number of Regular Tickets"
                 style={{width: "70%"}}
                 required
-                name="regular_no_of_tickets"
                 >
-                  <Input type='number'/>
+                  <Input min={0} name="regular_no_of_tickets" onChange={handleChange} value={formData.regular_no_of_tickets} type='number'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Description"
                 style={{width: "70%"}}
                 required
-                name="description"
                 >
-                  <TextArea rows={4} />
+                  <TextArea name="description" onChange={handleChange} value={formData.description} rows={4} />
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Banner Image"
                 style={{width: "70%"}}
                 required
-                name="banner_img"
                 >
-                  <Input type='text'/>
+                  <Input name="banner_img" onChange={handleChange} value={formData.banner_img} type='text'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Image Url"
                 style={{width: "70%"}}
                 required
-                name="image_url1"
                 >
-                  <Input type='text'/>
+                  <Input name="image_url1" onChange={handleChange} value={formData.image_url1} type='text'/>
                 </Form.Item>
                 <br />
                 <Form.Item 
                 label="Second Image Url"
                 style={{width: "70%"}}
                 required
-                name="image_url2"
                 >
-                  <Input type='text'/>
+                  <Input name="image_url2" onChange={handleChange} value={formData.image_url2} type='text'/>
                 </Form.Item>
 
               </Row>
@@ -201,7 +270,10 @@ const createvent = () => {
               <Form.Item 
               style={{width: "30%"}}
               >
-                <Button style={{width: "100%", 
+                <Button 
+                type='submit'
+                onClick={handleSubmit}
+                style={{width: "100%", 
                 backgroundColor: "#d1410a", 
                 color: "#fff",
                 height: 30,
