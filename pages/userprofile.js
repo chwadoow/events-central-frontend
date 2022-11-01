75764883158f5d48718702fc08050026f0aafae3
moses ngige <moses32ngigz@gmail.com>
2022-11-01 04:08:02 +0300

    fetch and display of user profile

--- pages/userprofile.js
+++ pages/userprofile.js
@@ -1,80 +1,160 @@
-import { Col, Row ,Card} from 'antd';
-import { AntDesignOutlined,EditOutlined,LogoutOutlined } from '@ant-design/icons';
-import { Avatar,Button } from 'antd';
-import {LeftSquareOutlined} from  '@ant-design/icons';
-import UserCard from '../components/UserCard';
 import {useEffect,useState} from 'react'
-function userprofile() {
-const [userData,setUserData] = useState([])
+import { Row, Col } from 'antd';
+import { Image } from 'antd';
+import { Carousel } from 'antd';
+import { Descriptions } from 'antd';
 
-  useEffect(()=>{
-    fetch(`http://localhost:3000/users/2`)
-    .then(response => response.json())
-    .then((data)=> {
-  setUserData(data)
-     
-    })
-    },[])
+
+
+
+import 'antd/dist/antd.css';
+const { Meta} = Card;
+
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+
+
+function UserProfile() {
+const [user,setUser]=useState(null)
+const [userProfile,setUserProfile]=useState([])
+    useEffect(() => {
+      // getter
+ setUser(  JSON.parse(window.localStorage.getItem('session')));   
+
+{user ?
+ fetch(`http://localhost:3000/user_profiles/${user.id}`)
+ .then(response => response.json())
+ .then((data)=> {
+setUserProfile(data)
+ })
+ :console.log("not yet")}
+ 
+
+    }, [])
+
+
+
+
   
+
+
+
+
 
 
   return (
-  <div
-  style={{width: "100%" ,
-  textAlign: "center",
-  }}
-  >
-
-
-
-  
-<Row 
-style={{width: "100%" ,
-textAlign: "center",
-justifyContent:"center"
-}}> 
- <Card
-    hoverable
+    <>
+    <Row>
+    <Col 
+    style={{width: "100%"}}  >
+  <Card
     style={{
-      background: "grey",
-      borderRadius: "50%",
-      width: "100px",
-      height: "100px",
-      }}>
-        avatar image
-     </Card>
-     
+      width: '100%' ,
+    }}
 
-</Row>
-<br></br>
-<Row
-style={{width: "100%" ,
-textAlign: "center",
-justifyContent:"center"
-}}> 
-{userData.username}
-
-</Row>
-
-<Row>
-  <UserCard userData={userData}/>
-</Row>
-<br></br>
-<Row>
-<Button 
-
-style={{width: "100%" ,
-textAlign: "center",
-justifyContent:"center"
-}}
-
-type="primary" icon={<LogoutOutlined />} size="large"/>
-
-</Row>
-</div>
+  >
+    <Avatar src="https://joeschmoe.io/api/v1/random" />
     
+  </Card>
+    </Col>
+   
+    
+  </Row>
 
+  <Row
+    style={{
+      width: "100%",
+      textAlign: 'center'
+      
+  
+    }}
+  >
+  <Descriptions
+      title="Hello,welcome to your profile info"
+      bordered
+      column={{
+        xxl: 6,
+        xl: 3,
+        lg: 3,
+        md: 3,
+        sm: 2,
+        xs: 1,
+      }}
+      style={{
+        width: "130%"
+      }}
+    >
+      <Descriptions.Item label="name">{userProfile.full_name} </Descriptions.Item>
+      <Descriptions.Item label="gender">{userProfile.gender}</Descriptions.Item>
+      <Descriptions.Item label="phone">{userProfile.phone_number}</Descriptions.Item>
+      <Descriptions.Item label='email'>{userProfile.email}</Descriptions.Item>
+      <Descriptions.Item label="bio">{userProfile.bio}</Descriptions.Item>
+     
+ 
+    </Descriptions>
+
+
+  </Row>
+     <Row>
+     {/* <Col xs={20} sm={20} md={20} lg={20} xl={20}
+ >
+   <Card
+    //  style={{
+    //    width: '70%' ,
+    //  }}
+ 
+   >
+   Edit
+     
+   </Card>
+     </Col> */}
+     <Col xs={24} sm={24} md={24} lg={24} xl={24}
+    >
+   <Card
+     style={{
+       textAlign: "center"
+     }}
+ 
+   >
+   <EditOutlined/>
+   Edit
+     
+   </Card>
+     </Col>
+    
+     
+   </Row>
+
+
+   <Row
+  //     style={{
+  //   textAlign: "center"
+  // }}
+   >
+<Col   
+
+
+>
+    <p>past Events</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    </Col>
+   </Row>
+   </>
   )
 }
 
-export default userprofile
+export default UserProfile
