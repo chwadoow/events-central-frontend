0dffb5cff47eefa7844115a27cc013b492473ff1
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-11-03 13:13:00 +0300

    Added data to user profile

--- components/RightMenu.js
+++ components/RightMenu.js
@@ -1,90 +1,90 @@
-import { useRouter } from 'next/router'
-import { UserOutlined } from '@ant-design/icons';
-import React, { useEffect, useState } from 'react';
-import { Menu, Avatar, Grid, Dropdown} from 'antd';
-
+import { useRouter } from "next/router";
+import { UserOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import { Menu, Avatar, Grid, Dropdown } from "antd";
 
 const { useBreakpoint } = Grid;
 
 const RightMenu = () => {
-  
-  const [userData, setUserData] = useState({})
-  useEffect(()=>{
-    const session = JSON.parse(localStorage.getItem("session"))
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    const session = JSON.parse(localStorage.getItem("session"));
     fetch(`http://localhost:3000/users/${session}`)
-    .then(response => response.json())
-    .then(data => setUserData(data))
-  },[])
+      .then((response) => response.json())
+      .then((data) => setUserData(data));
+  }, []);
 
-  let router= useRouter()
-function redirect() {
-  router.push('/')
-}
+  let router = useRouter();
+  function redirect() {
+    router.push("/");
+  }
 
-     const handleLogout = () => {
-        fetch('http://localhost:3000/logout', {method: "DELETE"})
-           .then(res => {
-             if (res.ok) {
-                localStorage.clear()
-                 redirect()
-                 alert("logged out")
-              }
-            })
-     } 
-  
-    
-    const { md } = useBreakpoint();
+  const handleLogout = () => {
+    fetch("http://localhost:3000/logout", { method: "DELETE" }).then((res) => {
+      if (res.ok) {
+        localStorage.clear();
+        redirect();
+        alert("logged out");
+      }
+    });
+  };
 
-    const menu = (
-      <Menu>
-        <Menu.Item>
-          <a href="/login">Login</a>
-        </Menu.Item>
+  const { md } = useBreakpoint();
 
-        <Menu.Item>
-          <a href="/userprofile">View Profile</a>
-        </Menu.Item>
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a href="/login">Login</a>
+      </Menu.Item>
 
-        <Menu.Item>
-          <a href="/viewevents">View Events</a>
-        </Menu.Item>
+      <Menu.Item>
+        <a href="/userprofile">View Profile</a>
+      </Menu.Item>
 
-        <Menu.Item>
-          <a onClick={handleLogout}>Logout</a>
-        </Menu.Item>
-      </Menu>
-    );
+      <Menu.Item>
+        <a href="/viewevents">View Events</a>
+      </Menu.Item>
 
-    return (
+      <Menu.Item>
+        <a onClick={handleLogout}>Logout</a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
     <Menu overflowedIndicator mode={md ? "horizontal" : "inline"}>
-       <Menu.Item >
-          <a href="/"><b>Home</b></a>
-        </Menu.Item>
-        
-        {userData.is_organiser ? (
-          <Menu.Item>
-            <a href="/createvent">
-              <b>Create an Event</b>
-            </a>
-          </Menu.Item>
-        ) : (
-          " "
-        )}
+      <Menu.Item>
+        <a href="/">
+          <b>Home</b>
+        </a>
+      </Menu.Item>
 
-        <Menu.Item >
-          <a href="/about"><b>About Us</b></a>
-        </Menu.Item>
+      {userData.is_organiser ? (
         <Menu.Item>
-          <div>
-            <Dropdown overlay={menu} trigger={["click"]} placement="bottomLeft">
-              {/* <Link  href={`/userprofile`}> */}
-              <Avatar size={35} icon={<UserOutlined />} />
-              {/* </Link> */}
-            </Dropdown>
-          </div>
+          <a href="/createvent">
+            <b>Create an Event</b>
+          </a>
         </Menu.Item>
-      </Menu>
-    );
-}
+      ) : (
+        " "
+      )}
+
+      <Menu.Item>
+        <a href="/about">
+          <b>About Us</b>
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <div>
+          <Dropdown overlay={menu} trigger={["click"]} placement="bottomLeft">
+            {/* <Link  href={`/userprofile`}> */}
+            <Avatar size={45} icon={<UserOutlined />} />
+            {/* </Link> */}
+          </Dropdown>
+        </div>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default RightMenu;
--- pages/userprofile.js
+++ pages/userprofile.js
@@ -1,283 +1,158 @@
-import {useEffect,useState} from 'react'
-import { Row, Col } from 'antd';
-import { Image } from 'antd';
-import { Carousel } from 'antd';
-import { Descriptions } from 'antd';
-import { Layout } from 'antd';
-import { Button } from 'antd';
+import { useEffect, useState } from "react";
+import { Row, Col } from "antd";
+import { Image } from "antd";
+import { Carousel } from "antd";
+import { Descriptions } from "antd";
+import { Layout } from "antd";
+import { Button } from "antd";
 
 const { Header, Footer, Sider, Content } = Layout;
-import { Empty } from 'antd';
+import { Empty } from "antd";
 
 import { Table } from "antd";
 
+import "antd/dist/antd.css";
+const { Meta } = Card;
 
-import 'antd/dist/antd.css';
-const { Meta} = Card;
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
 
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
+function UserProfile(homebanners) {
+  const [userProfile, setUserProfile] = useState([]);
 
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    const session = JSON.parse(localStorage.getItem("session"));
 
-function UserProfile( homebanners) {
+    fetch(`http://localhost:3000/users/${session}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      });
+  }, []);
 
-const [userProfile,setUserProfile]=useState([])
-
-const[userData,setUserData]=useState('')
-
-  
-const [user, setUser] = useState({})
-useEffect(()=>{
-  const session = JSON.parse(localStorage.getItem("session"))
-
-
-
-  fetch(`http://localhost:3000/user_profiles/${session}`)
- .then(response => response.json())
- .then((data)=> {
-setUserProfile(data)
- })
-
-},[])
-
-useEffect(()=>{
-
-  const session = JSON.parse(localStorage.getItem("session"))
-
-fetch(`http://localhost:3000/users/${session}`)
-.then(response => response.json())
-.then((data)=> {
-setUserData(data)})},[])
-
-
-
-console.log(userData.events)
+  console.log(userData);
 
   return (
     <>
-
-<Layout  >
-      <Header  style={{backgroundColor: "white"}}>
-
-<div    style={{float: "left"}}>
-
-  Welcome 
-</div>
-
-<div   style={{float: "right"}}>
-
-<EditOutlined/>
-edit
-
-</div>
-
-
-      </Header>
-
-
       <Layout>
+        <Header style={{ backgroundColor: "white" }}>
+          <div style={{ float: "left" }}>{`Welcome  ${userData.username}`}</div>
+          <div style={{ float: "right" }}>
+            <EditOutlined />
+            edit
+          </div>
+        </Header>
 
+        <Layout>
+          <Content style={{ backgroundColor: "#ffffff" }}>
+            <Row>
+              <Col
+                span={12}
+                style={{ textAlign: "center", columnRuleStyle: "dotted" }}
+              >
+                <Image src={userData.user_profile?.avatar_img} />
+              </Col>
 
- <Content   style={{backgroundColor: "#ffffff"}}>
+              <Col span={12} style={{ boxShadow: "  1px" }}>
+                <Row>
+                  <Col span={12}>
+                    <Card
+                      title={`Name: ${userData.user_profile?.full_name}`}
+                      bordered={true}
+                    >{`Email: ${userData.email}`}</Card>
+                  </Col>
 
- {/* <Row >
-        <Col span={24}>
-          <Carousel 
-          autoplay
-          autoplaySpeed={3000}
-          easing
-          effect="scrollx"
-          dots={false}
-          draggable
-          pauseOnHover={true}
-          >
-              
-             
-                  <>
-                    <img 
-                      src={'https://p4.wallpaperbetter.com/wallpaper/562/719/645/bar-club-dance-dancing-wallpaper-preview.jpg'}
-                   
-                      style={{
-                        width: "100%",
-                        height: "50vh",
-                        padding: '10px'
-                      }}
-                    />
-                  </>
-                
-          </Carousel>
-        </Col>
-      </Row> */}
+                  <Col span={12}>
+                    <Card
+                      title={`Phone Number: ${userData.user_profile?.phone_number}`}
+                      bordered={true}
+                    >{`Gender: ${userData.user_profile?.gender}`}</Card>
+                  </Col>
+                </Row>
+                <Row style={{ marginTop: "3rem", justifyContent: "center" }}>
+                  <Card
+                    span={24}
+                    bordered={true}
+                  >{`Bio : ${userData.user_profile?.bio}`}</Card>
+                </Row>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
 
-      <Row >
+        <Footer style={{ textAlign: "center", backgroundColor: "#ffffff" }}>
+          <Button type="dashed" block>
+            View attended events
+          </Button>
 
-<Col span={24} style={{textAlign:'center',columnRuleStyle: 'dotted'}}>
- 
-<Image src={userProfile.avatar_img}/>
+          <br></br>
 
-</Col>
+          <Row justify="center" align="middle">
+            <Col span={24}>
+              <div
+                style={{
+                  marginRight: 10,
+                  marginLeft: 10,
+                  borderRadius: 20,
+                  gap: 20,
+                  justifyContent: "center",
+                  flexWrap: "wrap",
+                  display: "inline-flex",
+                  flexDirection: "row",
+                }}
+              >
+                {userData ? (
+                  userData.events.map((event) => {
+                    return (
+                      <div key={event.id}>
+                        <div>
+                          <Card
+                            style={{
+                              textAlign: "left",
+                              width: 280,
+                              maxHeight: 600,
+                              padding: 2,
+                              cursor: "pointer",
+                            }}
+                            cover={
+                              <img
+                                alt={event.title}
+                                src={event.banner_img}
+                                height="200px"
+                              />
+                            }
+                            hoverable
+                          >
+                            <div>
+                              <h1
+                                style={{ fontWeight: "bolder", fontSize: "15" }}
+                              >
+                                {event.title}
+                              </h1>
 
-<Col span={24}  style={{  boxShadow:'  1px'}}>
-
-    <Row>
-        <Col span={12}>
-            <Card title="name" bordered={true}   >
-                       {userProfile.full_name}
-            </Card>
-
-        </Col>
-       
-        <Col span={12}>
-            <Card title="phone" bordered={true}>
-                       {userProfile.phone_number}
-            </Card>
-
-        </Col>
-      </Row>
-      <Row>
-        <Col span={12}>
-            <Card title="gender" bordered={true}>
-                       {userProfile.gender}
-            </Card>
-
-        </Col>
-
-        <Col span={12}>
-            <Card title="bio" bordered={true}>
-                       {userProfile.bio}
-            </Card>
-
-        </Col>
-      </Row>
-
-
-
-
-</Col>
-
-
-
-</Row> 
-
-  
-
- </Content>
-      </Layout>
-
-
-      <Footer
-        style={{textAlign:'center', 
-        backgroundColor: '#ffffff' }}>
-
-<Button type="dashed" block  >
-      View attended events
-    </Button>
-
-<br></br>
-
-
-
-    <Row justify="center" align="middle">
-      <Col span={24}>
-        <div style={{ marginRight: 10, marginLeft: 10, borderRadius: 20, gap: 20, justifyContent: "center", flexWrap: "wrap", display: "inline-flex", flexDirection: 'row' }}>
-          {userData? userData.events.map((event) => {
-              return (
-                <div key={event.id}>
-                
-                 
-                      <div >
-
-                        <Card style={{ 
-                          textAlign: "left", 
-                          width: 280, 
-                          maxHeight: 600, 
-                          padding: 2, 
-                          cursor: "pointer",
-                          }}
-
-                          cover={<img alt={event.title} src={event.banner_img} height="200px"/>}
-                          hoverable
-                        >
-                          <div>
-                            <h1 style={{fontWeight: "bolder", fontSize: "15"}}>{event.title}</h1>
-                         
-                           
-                              
-                           
-                            <p>{event.location}</p>
-                          </div>
-                          
-                        </Card>
-
+                              <p>{event.location}</p>
+                            </div>
+                          </Card>
+                        </div>
+                        &nbsp;
                       </div>
-               
-                  &nbsp;
-                  
-                </div>
-              );
-          }) :  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
-        </div>
-      </Col>
-    </Row>
-      
-      </Footer>
-    </Layout>
-  
-
+                    );
+                  })
+                ) : (
+                  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                )}
+              </div>
+            </Col>
+          </Row>
+        </Footer>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default UserProfile
-
-
-
-{/* 
- <Row >
-
-      <Col span={7} style={{textAlign:'center'}}>
-      <Image src={userProfile.avatar_img}/>
-      
-      </Col>
-
-      <Col span={14}  style={{  boxShadow:'  2px 2px 2px 2px'}}>
-
-          <Row>
-              <Col span={12}>
-                  <Card title="name" bordered={true}   >
-                             {userProfile.full_name}
-                  </Card>
-
-              </Col>
-             
-              <Col span={12}>
-                  <Card title="phone" bordered={true}>
-                             {userProfile.phone_number}
-                  </Card>
-
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                  <Card title="gender" bordered={true}>
-                             {userProfile.gender}
-                  </Card>
-
-              </Col>
-
-              <Col span={12}>
-                  <Card title="bio" bordered={true}>
-                             {userProfile.bio}
-                  </Card>
-
-              </Col>
-            </Row>
-
-
-
-
-      </Col>
-
-
-      
-</Row> */}
+export default UserProfile;
