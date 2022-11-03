b320cce4a0aa5b4d975445f00b7a80987c45f091
moses ngige <moses32ngigz@gmail.com>
2022-11-03 20:39:07 +0300

    can update user profile details

--- pages/userprofile.js
+++ pages/userprofile.js
@@ -9,7 +9,7 @@ import { Button } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 import { Empty } from 'antd';
 
-import { Table } from "antd";
+import { Table,Form , Input} from "antd";
 
 
 import 'antd/dist/antd.css';
@@ -27,6 +27,15 @@ const [userProfile,setUserProfile]=useState([])
 
 const[userData,setUserData]=useState('')
 
+const [formData, setFormData] = useState({
+full_name:"",
+phone_number:'',
+gender:"",
+avatar_img:"",
+bio:"",
+user_id:""
+})
+
   
 const [user, setUser] = useState({})
 useEffect(()=>{
@@ -37,7 +46,7 @@ useEffect(()=>{
   fetch(`http://localhost:3000/user_profiles/${session}`)
  .then(response => response.json())
  .then((data)=> {
-setUserProfile(data)
+setFormData(data)
  })
 
 },[])
@@ -53,7 +62,44 @@ setUserData(data)})},[])
 
 
 
-console.log(userData.events)
+console.log(formData)
+
+function handleChange(e){
+  setFormData({
+      ...formData, [e.target.name]: e.target.value,
+  });
+}
+
+
+function handleSubmit(e){
+  e.preventDefault();
+ 
+		fetch(`http://localhost:3000/user_profiles/${userData.id}`, {
+			method: 'PUT',
+			body: JSON.stringify({
+full_name: formData.full_name,
+gender: formData.gender,
+phone_number: formData.phone_number,
+bio: formData.bio,
+user_id: formData.user_id,
+avatar_img: formData.avatar_img
+			}),
+			headers: {
+			  "Content-type": "application/json"
+			}
+		}).then(response => {
+						return response.json()
+		}).then(json => {
+			console.log(json)
+		setFormData(json)
+    setShowForm(!showForm)
+			});
+		}
+  
+
+
+
+
 
   return (
     <>
@@ -91,7 +137,7 @@ edit
 
 <Col span={24} style={{textAlign:'center',columnRuleStyle: 'dotted'}}>
  
-<Image src={userProfile.avatar_img} />
+<Image src={formData.avatar_img} />
 
 </Col>
 
@@ -100,14 +146,14 @@ edit
     <Row>
         <Col span={12}>
             <Card title="name" bordered={true}   >
-                       {userProfile.full_name}
+                       {formData.full_name}
             </Card>
 
         </Col>
        
         <Col span={12}>
             <Card title="phone" bordered={true}>
-                       {userProfile.phone_number}
+                       {formData.phone_number}
             </Card>
 
         </Col>
@@ -115,14 +161,14 @@ edit
       <Row>
         <Col span={12}>
             <Card title="gender" bordered={true}>
-                       {userProfile.gender}
+                       {formData.gender}
             </Card>
 
         </Col>
 
         <Col span={12}>
             <Card title="bio" bordered={true}>
-                       {userProfile.bio}
+                       { formData.bio}
             </Card>
 
         </Col>
@@ -205,6 +251,92 @@ edit
     : 
     
 <>
+
+
+
+
+
+<Form
+            layout="vertical"
+            size='default'
+            onSubmit={(e) => handleSubmit(e)}
+            style={{width: "100%", marginLeft: "30%", marginRight: "30%"}}
+            >
+
+                <Form.Item
+                  label="full Name"
+                  style={{width: "100%"}}
+                  required
+                >
+                  <Input onChange={handleChange} value={formData.full_name} name="full_name" placeholder="eg. Joe Doe"/>
+                </Form.Item>
+
+
+
+                <Form.Item
+                  label="phone"
+                  style={{width: "100%"}}
+                  required
+                >
+                  <Input onChange={handleChange} value={formData.phone_number} name="phone_number" placeholder="eg. Joe Doe"/>
+                </Form.Item>
+
+                <Form.Item
+                  label="bio"
+                  style={{width: "100%"}}
+                  required
+                >
+                  <Input onChange={handleChange} value={formData.bio} name="bio" placeholder="enter your bio"/>
+                </Form.Item>
+
+
+                <Form.Item 
+                label="gender"
+                style={{width: "100%"}}
+                required
+                >
+                  <select
+                  name='gender'
+                  onChange={handleChange}
+                  style={{width: "100%"}}
+                  >
+                    <option value="">--Please choose your gender--</option>
+                    
+                        <option value='male' >male</option>
+                        <option value='female' >female</option>
+                  
+                  </select>
+                </Form.Item>
+
+                    <Form.Item 
+                    style={{width: "30%"}}
+                    >
+                      <Button 
+                      type='submit'
+                      onClick={(e) => handleSubmit(e)}
+                      style={{width: "100%", 
+                      backgroundColor: "#d1410a", 
+                      color: "#fff",
+                      height: 50,
+                      borderRadius: 10
+                      }}>
+                        Submit
+                      </Button>
+                    </Form.Item>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </Form>
 
 
 </>
