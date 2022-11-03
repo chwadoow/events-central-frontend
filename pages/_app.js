f385243ad975c3ed6000912e68a5cb2658892157
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-11-03 18:43:22 +0300

    Committed useEffect on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,38 @@
-import 'antd/dist/antd.css'; 
-import '../styles/globals.css';
+import "antd/dist/antd.css";
+import "../styles/globals.css";
 import "../styles/login.css";
-import { useRouter } from 'next/router';
-import {useEffect} from "react";
-import Navbar from '../components/Navbar';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  const router= useRouter();
+  const router = useRouter();
 
   useEffect(() => {
-    const session = localStorage.getItem('session')
-    console.log(session)
-    if(session !== null){
-      fetch("http://localhost:3000/me", {
-        headers: {
-          Authorization: "Bearer " + session,
-        },
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        window.localStorage('session', JSON.stringify(data));
-      })
-    } else {
-      router.push('/login');
-    }
-    }, []);
+    // const session = localStorage.getItem('session')
+    // console.log(session)
+    // if(session !== null){
+    //   fetch("http://localhost:3000/me", {
+    //     headers: {
+    //       Authorization: "Bearer " + session,
+    //     },
+    //   })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //     window.localStorage('session', JSON.stringify(data));
+    //   })
+    // } else {
+    //   router.push('/login');
+    // }
+  }, []);
 
-   
-
-  return( 
-    <div className='app'>
-    <Navbar/>
-    <Component {...pageProps} />
+  return (
+    <div className="app">
+      <Navbar />
+      <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
