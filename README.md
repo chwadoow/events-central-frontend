8f059f85747b301a537d660cd48af3c3c30333ab
francis <franciskibira017@gmail.com>
2022-11-04 10:51:20 +0300

    readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Title 
 Event Ticket and Management system.
 
+# Founders
+The builders of this project are as follows: Mutagah Kihahu,Jessica Jennifer,Keith Wesley,Moses Ngige and Francis Kibira
+
 # Description
 
 This project is a simple responsive front end website that has next jsas the front end.
@@ -11,6 +14,5 @@ clone to the github Repo git@github.com:chwadoow/events-central-frontend.git Unz
 # License
  MIT License Copyright (c) [2022] [francis kibira,mutagah kihahu,moses ngige,keith wesleh and jeska jenniffer] Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software")to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions.
 
-# Founders
-The builders of this project are as follows: Mutagah Kihahu,Jessica Jennifer,Keith Wesley,Moses Ngige and Francis Kibira
+
 
--- pages/login.js
+++ pages/login.js
@@ -1,119 +1,115 @@
-import Link from "next/link"
-import {useState} from "react"
-import { useRouter } from 'next/router'
-import { Form, Input, Button, Checkbox , notification} from '@ant-design/';
-import { NoStyleItemContext } from "antd/lib/form/context"
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+// import { Form, Input, Button, Checkbox, notification } from "@ant-design/";
+import { NoStyleItemContext } from "antd/lib/form/context";
 
+function Login({ user, setUser }) {
+  let router = useRouter();
 
-function Login({user, setUser}){
-  let router= useRouter()
-
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState([])
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState([]);
   // const [user, setUser] = useState({})
 
-
-  function handleSubmit(event){
+  function handleSubmit(event) {
     // event.preventDefault();
-    console.log(username,password) 
+    console.log(username, password);
 
-    fetch("http://localhost:3000/login",{
+    fetch("http://localhost:3000/login", {
       method: "POST",
       headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json",
       },
-      body :JSON.stringify({
+      body: JSON.stringify({
         username: username,
-        password: password 
-      })
-    }).then((res)=> {
-      if(res.ok){
-        res.json().then((data)=>
-        {
-          window.localStorage.setItem('session', JSON.stringify(data.id));
-          console.log("logged in ? ", data.id) 
+        password: password,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          window.localStorage.setItem("session", JSON.stringify(data.id));
+          console.log("logged in ? ", data.id);
 
-          alert("You have loggged in successfully")
+          alert("You have loggged in successfully");
 
- 
-          router.push('/')
-        } 
-         
-        )
-
-      } else{
-        res.json().then((e)=> 
-        setError([e.error]))
+          router.push("/");
+        });
+      } else {
+        res.json().then((e) => setError([e.error]));
       }
-    })
-}
+    });
+  }
 
-
-
-    return (
-        <div className="login-page">
+  return (
+    <div className="login-page">
       <div className="login-box">
         <div className="illustration-wrapper">
-          <img src="https://images.pexels.com/photos/382297/pexels-photo-382297.jpeg?cs=srgb&dl=pexels-salah-alawadhi-382297.jpg&fm=jpg" alt="Login"/>
+          <img
+            src="https://images.pexels.com/photos/382297/pexels-photo-382297.jpeg?cs=srgb&dl=pexels-salah-alawadhi-382297.jpg&fm=jpg"
+            alt="Login"
+          />
         </div>
-        
-     
 
-      {/* <Col xs={24} md={12}> */}
-      <Form onFinish={handleSubmit} name="login-form">
+        {/* <Col xs={24} md={12}> */}
+        <Form onFinish={handleSubmit} name="login-form">
           <p className="form-title">LOG IN</p>
           <Form.Item
             name="username"
-            rules={[{required: true, message: 'Please input your username!'}]}
+            rules={[{ required: true, message: "Please input your username!" }]}
           >
             <Input
               placeholder="Input username"
               name="username"
               value={username}
-              onChange={(event)=> setUsername(event.target.value)}
+              onChange={(event) => setUsername(event.target.value)}
             />
           </Form.Item>
 
           <Form.Item
-          placeholder="input password"
+            placeholder="input password"
             name="password"
-            rules={[{required: true, message: 'Please input your password!'}]}
+            rules={[{ required: true, message: "Please input your password!" }]}
           >
-            <Input.Password 
+            <Input.Password
               placeholder="Password"
               name="password"
               value={password}
-              onChange={(event)=> setPassword(event.target.value)}
+              onChange={(event) => setPassword(event.target.value)}
             />
           </Form.Item>
 
           {error.length > 0 && (
-                                <ul style={{color:"red"}}className="fs-5">
-                                    {error.map(error => <li key={error}>{error}</li>)}
-                                </ul>
-                            )}
+            <ul style={{ color: "red" }} className="fs-5">
+              {error.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )}
 
           <Form.Item>
-            <Button 
-            // onClick={handleSubmit}
-            onClick={() => {
-              notification.open({
-                message: 'Login Status',
-                description:
-                  'Login Successfully!',
-                duration: 1.5,
-              });
-            }}
-            htmlType="submit" className="login-form-button" >
+            <Button
+              // onClick={handleSubmit}
+              onClick={() => {
+                notification.open({
+                  message: "Login Status",
+                  description: "Login Successfully!",
+                  duration: 1.5,
+                });
+              }}
+              htmlType="submit"
+              className="login-form-button"
+            >
               LOGIN
             </Button>
           </Form.Item>
-          <p>If you dont have an account? <Link href="/signup">SignUp</Link> </p>
+          <p>
+            If you dont have an account? <Link href="/signup">SignUp</Link>{" "}
+          </p>
         </Form>
-      {/* </Col> */}
+        {/* </Col> */}
       </div>
-      </div>
-    )
-    }
-    export default Login
+    </div>
+  );
+}
+export default Login;
